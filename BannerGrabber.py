2252fea26ec60bfe78c1f6fd7f91aeccd7b90b31
Stoic Scythe <8756.987pasth@gmail.com>
2025-07-04 17:58:40 +0800

    Update BannerGrabber.py

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -2,13 +2,9 @@
 import socket
 
 try:
-    with socket.socket() as s: # set s as socket
-        
+    with socket.socket() as s:  # set s as socket
         s.connect(("127.0.0.1", 22))
-        
         answer = s.recv(1024)
-  
-        print(answer.decode()) #.decode() converts a bytes object into a string
-
-except ConnectionRefusedError: # 'except' catches the error if the server is not running
-    print("Connection closed")
+        print(answer.decode())
+except ConnectionRefusedError:  # 'except' catches the error if the server is not running
+    print("Connection failed")
